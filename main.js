68ac15792e8e87fa9ffa8fc8f4111cd8573acceb
jlucaspains <jlucaspains@gmail.com>
2025-01-19 15:28:21 -0700

    Removed main file

--- main.js
+++
@@ -1,7 +0,0 @@
-import { parseIngredient } from "./src/index.js";
-
-const result = parseIngredient("300g/10½oz onions, thinly sliced", "en-US", {
-  includeAlternativeUnits: true,
-  includeExtra: true,
-});
-console.log(result);
